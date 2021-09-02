29fdf0c3f160fda67c94c22de11d3479fdb9bd29
12abdulmalek <abdul35-585@diu.edu.bd>
2021-09-02 08:48:54 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+const bookList = document.getElementById('book-list');
+
+const searchBookName = searchResult => {
+    const search=searchResult.value;
+  
+    const url = `http://openlibrary.org/search.json?q=${search}`;
+    return url;
+}
+
+const books = () =>{
+    const searchText=document.getElementById('search-text');
+      const url=searchBookName(searchText);
+      searchText.value='';
+   // const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>booksDetails(data));
+}
+const booksDetails = books =>{
+   //  console.log(book.key);
+   let count=0;
+   console.log(books.numFound);
+   let c=0;
+   bookList.textContent='';
+   const book=books.docs;
+   if(book.length===0){
+       document.getElementById('show-result').style.display='block';
+       document.getElementById('count-book').style.display='none';
+   }
+    book.forEach(item => {
+        count++;
+        const booksUrl = `https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg`;
+        // console.log(item ,c++);
+        //console.log(burl);
+        const div=document.createElement('div');
+        div.innerHTML=`
+        <div class="col">
+         <div class="card"> 
+            <img  src="${booksUrl}"  alt="...">
+            
+       <div class="card-body">
+       <h5 class="card-title">${item.title}</h5>
+       <p class="card-text">${item.author_name}</p>
+     </div>
+     <div class="card-footer">
+      <p> ${item.first_publish_year} </br> ${count}</p>
+     </div>
+   </div>
+          </div>
+        `;
+        bookList.appendChild(div);
+        document.getElementById('show-result').style.display='none';
+        document.getElementById('count-book').style.display='block';
+        document.getElementById('count-book').innerText=`displaying 1 - ${count} of about ${books.numFound} results`;
+    });
+    // console.log(book.docs[0].title);
+}
